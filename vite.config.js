f24b1f4a430aaba5de9d265d3abc746136dea565
Patypatii <patrickwambugu707@gmail.com>
2025-07-18 21:50:52 +0300

    configured vite url

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/patrick-portfolio/',
+    base: '/dev-portfolio/',
     plugins: [react()],
     build: {
         rollupOptions: {
